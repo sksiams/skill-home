1c006bbd65bf9403e13038a507ec3393e37ec5af
sksiams <71906915+sksiams@users.noreply.github.com>
2020-09-28 03:06:07 +0600

    Update custom.js

--- js-document/custom.js
+++ js-document/custom.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function(){
+ jQuery(document).ready(function(){
 	
 	jQuery('.srch-icon').click(
 	
 		function(){
-			jQuery('.menu-area ul').css({'margin-left':'24%'});
+			jQuery('.menu-area ul').css({'margin-left':'25%'});
 			jQuery('.srch-inpt').fadeIn(
 				function(){
 					
@@ -29,57 +29,61 @@ jQuery(document).ready(function(){
 	);
 	
 
-	jQuery(window).scroll(
+	jQuery('.rspnsv-bar').click( 
 	
 		function(){
 			
-			if(jQuery(window).scrollTop() >1700){
-				
-				jQuery('.fclty-right>.std-pic').css({'transform':'scale(1)','opacity':'1'});
- 
-	
-			}
-			else{
-				
-				jQuery('.fclty-right>.std-pic').css({'transform':'scale(.2)','opacity':'.1'});
-				
-				
-			}
-
-			if(jQuery(window).scrollTop() >2200){
-				
-				jQuery('.fclity-dygrm').css({'transform':'scale(1)','opacity':'1'});
-				jQuery('.fclity-ststc').css({'transform':'scale(1)','opacity':'1'});
-	
-			}
-			else{
-				
-				jQuery('.fclity-dygrm').css({'transform':'scale(.2)','opacity':'.2'});
-				jQuery('.fclity-ststc').css({'transform':'scale(.2)','opacity':'.2'});
-				
-			}
-			
+			jQuery('.rspnsv-menu').fadeToggle();
 			
 			
 			
 		}
 	
 	
+	);
+	
+	
+	
+	
+	
+	
+	
+	jQuery(window).scroll( 
+	
+		function(){
+			
+			if(jQuery(window).scrollTop() > 3200 ){
+				
+				jQuery('.fclty-right>.std-pic').css({'transform':'scale(1)','opacity':'1'});
+				
+				
+			}
+			else{
+				
+				jQuery('.fclty-right>.std-pic').css({'transform':'scale(.1)','opacity':'.1'});
+				
+				
+			}
+			
+			if(jQuery(window).scrollTop() > 3700 ){
+				
+				jQuery('.fclity-dygrm').css({'transform':'scale(1)','opacity':'1'});
+				jQuery('.fclity-ststc').css({'transform':'scaleX(1)','opacity':'1'});
+				
+				
+			}
+			else{
+				
+				jQuery('.fclity-dygrm').css({'transform':'scale(.1)','opacity':'.1'});
+				jQuery('.fclity-ststc').css({'transform':'scaleX(.1)','opacity':'.1'});
+				
+				
+			}			
+			
+		}
 	
 	);
-
-
-	
  
-
-
-jQuery(function(){
-
-	jQuery('.bck').backToTop({iconName:'fas fa-chevron-up',trigger : 300,fxName :'fade',fxTransitionDuration : 300,scrollDuration : 300});  
-
-});
-
-
 
 
 
